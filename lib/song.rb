03a385bb29734fef4afac07b1b37054ebf9d8d6e
elisa <elisaopalka@gmail.com>
2020-11-21 00:47:57 -0500

    commit tree c9add035d6c72dfd15680eba15f3e0ab5305e857

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,54 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect{|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(file_name)
+    song = self.new
+    split_string = file_name.split(" - ")
+    song.artist_name = split_string[0]
+    song.name = split_string[1].gsub(".mp3","")
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    song = self.create
+    split_string = file_name.split(" - ")
+    song.artist_name = split_string[0]
+    song.name = split_string[1].gsub(".mp3","")
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 end
